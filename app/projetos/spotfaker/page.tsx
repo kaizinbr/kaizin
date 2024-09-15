de7ffa69666f809c4f1689334f3cd868a7779e9f
Kaizin <kaiolucas1812@gmail.com>
2024-09-15 10:47:46 -0300

    onde q ta dando erro no window vercel?

--- app/projetos/spotfaker/page.tsx
+++ app/projetos/spotfaker/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FiExternalLink } from "react-icons/fi";
-import LocomotiveScroll from 'locomotive-scroll';
 
-const locomotiveScroll = new LocomotiveScroll();
 
 export default function Projetos() {
     return (
