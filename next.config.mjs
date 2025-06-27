f554053144f333faea7fa5757b1e239ee4e76dd0
Kaizin <kaiolucas1812@gmail.com>
2025-06-27 08:32:31 -0300

    redirect in next config test

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,30 @@
 const nextConfig = {
     images: {
         unoptimized: true,
-    }
+    },
+    async redirects() {
+        return [
+            // Basic redirect
+            {
+                source: "/sitemap",
+                destination: "https://drive.google.com/file/d/1fgg5D4OkknrzAI-zNuab-yfc3TcdEQ0S/view?usp=sharing",
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
 
 // import million from "million/compiler";
- 
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
 // };
- 
+
 // const millionConfig = {
 //   auto: true,// if you're using RSC: auto: { rsc: true },
 // };
- 
+
 // export default million.next(nextConfig, millionConfig);
